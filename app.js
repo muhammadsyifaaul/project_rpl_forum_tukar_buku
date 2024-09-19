583f38abcc6c4f6a4b022741f76b803964e3fd89
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2024-09-19 14:10:39 +0700

    express set

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+const path =require ('path')
 const express = require('express')
 const app = express()
 
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.set('view engine','ejs')
+
+app.set('views',path.join(__dirname,'src/views'))
+
+app.use(express.static(path.join(__dirname,'src/public')))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('auth/register')
+})
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
+app.get('/login',(req,res) => {
+    res.render('auth/login')
 })
 
 

cd56b4f87a201f2fe4e96e4c9b08403c2c23492a
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2024-10-17 23:20:27 +0700

    socket io integration

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const path =require ('path')
 const express = require('express')
-const expressLayouts= require('express-ejs-layouts')
+const http = require('http');
 const app = express()
+const socketIo = require('socket.io');
+const server = http.createServer(app);  // Server HTTP yang sama
+const io = socketIo(server);  // Hubungkan ke server HTTP yang sama
+const expressLayouts= require('express-ejs-layouts')
+
 const session = require('express-session');
 const authRoutes = require('./src/routes/authRoutes')
 const userRoutes = require('./src/routes/userRoutes')
@@ -27,10 +32,24 @@ app.use(session({
 connectDb()
 app.use(authRoutes)
 app.use(userRoutes)
+io.on('connection', (socket) => {
+    console.log('User connected');
+  
+    // Terima pesan dari client
+    socket.on('chatMessage', (msg) => {
+      console.log('Message received from client: ' + msg);  // Debugging received message
+      io.emit('chatMessage', msg);  // Emit kembali ke semua client
+    });
+    
+    // Saat user disconnect
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+  });
+  
 
 
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
 
-
-app.listen(3000, () => {
-    console.log('server running perfectly')
-})
